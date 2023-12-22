01795152c234e5e5da0a4a590bfc344d4b307a52
Miłosz Pigłas <milosz@archeocs.com>
2023-12-22 13:44:06 +0100

    first commit

---
+++ src/main/java/com/example/demo/MyConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties("config")
+@Configuration
+public class MyConfig {
+
+  private String message;
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String newMessage) {
+    System.out.println("Old value=%s, New value=%s".formatted(this.message, newMessage));
+    this.message = newMessage;
+  }
+}
